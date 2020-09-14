588feaaa1f4b16484c00116f6d99cf13ba49c6d6
Kenny <kenny_hung@mail.chungyo.net>
2020-09-14 14:02:03 +0800

    套用銀行和網站常數至 store 上

---
+++ src/settings/consts/webs.ts
@@ -0,0 +1,31 @@
+import Web from '@/settings/interfaces/web'
+
+export const LOCAL_WEB: Web = {
+    name: 'LOCAL',
+    uri: 'http://172.16.155.158:8080',
+    token: 'robot:token'
+};
+
+export const DEVELOP_WEB: Web = {
+    name: 'DEVELOP',
+    uri: 'https://outgoing-api-develop.rd2dev.io',
+    token: 'robot:8VeYjZkD42hYmB56ubGuAvNvQA4ZuUwaeCsUJrtFBdFwzL5btGq2r69rDwN3msqx'
+};
+
+export const ESB_WEB: Web = {
+    name: 'ESB',
+    uri: 'https://api.assist2pay.com',
+    token: 'robot:LQVFTS9jp2CyBHwje6qbmEthLdRTkTURSFzjTfmxbxARg3Bh8MghZu5xx7Ys76Rb'
+};
+
+export const DOUBLE_DRAGON_WEB: Web = {
+    name: 'DOUBLE_DRAGON',
+    uri: 'https://api.goldenflow.net',
+    token: 'robot:Jyh2zgQMmRWqWkKRTrp5u3S6BfhjUnZcmqKhRRcAP9aRdvzJsRFZ98XKYhzRvnnR'
+};
+
+export const CG_PAY_WEB: Web = {
+    name: 'CG_PAY',
+    uri: 'https://api.fast168pay.com',
+    token: 'robot:ZhhawrFNCRHmAWRHDyJ3nnst7cfVDxGvEAfZBLr5dX2rwHXY7hS5vU8tHddbmxgR'
+};
--- src/settings/store/state.ts
+++ src/settings/store/state.ts
@@ -1,5 +1,7 @@
 import Bank from '@/settings/interfaces/bank'
+import { ABOC, FJIB, ICBK, MSBC, PCBC, SPDB, SZDB } from '@/settings/consts/banks'
 import Web from '@/settings/interfaces/web'
+import { CG_PAY_WEB, DEVELOP_WEB, DOUBLE_DRAGON_WEB, ESB_WEB, LOCAL_WEB } from '@/settings/consts/webs';
 
 export interface SettingsState {
     webs: Web[];
@@ -8,8 +10,22 @@ export interface SettingsState {
 
 const state = (): SettingsState => {
     return {
-        webs: [],
-        banks: []
+        webs: [
+            LOCAL_WEB,
+            DEVELOP_WEB,
+            ESB_WEB,
+            DOUBLE_DRAGON_WEB,
+            CG_PAY_WEB
+        ],
+        banks: [
+            ABOC,
+            ICBK,
+            PCBC,
+            MSBC,
+            SPDB,
+            SZDB,
+            FJIB
+        ]
     };
 };
 
