07448c2c35072a24168b954d5b91b089facf3ecf
Kenny <kenny_hung@mail.chungyo.net>
2020-09-17 12:24:14 +0800

    套用腳本 store 至上傳腳本表單元件上

--- src/script/components/UploadForm.vue
+++ src/script/components/UploadForm.vue
@@ -1,30 +1,30 @@
 <template>
-    <a-form-model :model="form" :label-col="{ span: 2 }" :wrapper-col="{ span: 20 }">
+    <a-form-model :model="uploadForm" :label-col="{ span: 2 }" :wrapper-col="{ span: 20 }">
         <a-form-model-item label="網站">
-            <a-checkbox-group v-model="form.webs">
+            <a-checkbox-group v-model="uploadForm.webs">
                 <a-checkbox v-for="web in webs" :key="web.name" :value="web">
                     {{ web.name }}
                 </a-checkbox>
             </a-checkbox-group>
         </a-form-model-item>
         <a-form-model-item label="腳本">
-            <a-input v-model="form.script" type="textarea" @change="handleScriptChange" />
-            <a-tag color="blue" v-if="form.bank !== undefined">
-                {{ form.bank.name }}
+            <a-input v-model="uploadForm.script" type="textarea" @change="handleScriptChange" />
+            <a-tag color="blue" v-if="uploadForm.bank !== undefined">
+                {{ uploadForm.bank.name }}
             </a-tag>
         </a-form-model-item>
         <a-form-model-item label="備註">
             <a-input-group compact>
-                <a-select v-model="form.notePrefix">
+                <a-select v-model="uploadForm.notePrefix">
                     <a-select-option v-for="web in webs" :key="web.name" :value="web.name">
                         {{ web.name }}
                     </a-select-option>
                 </a-select>
-                <a-input v-model="form.notePostfix" class="not-postfix" />
+                <a-input v-model="uploadForm.notePostfix" class="not-postfix" />
             </a-input-group>
         </a-form-model-item>
         <a-form-model-item label="穩定版本">
-            <a-switch v-model="form.isStable" />
+            <a-switch v-model="uploadForm.isStable" />
         </a-form-model-item>
         <a-form-model-item :wrapper-col="{ offset: 2 }">
             <a-button type="primary" @click="submit"> 上傳 </a-button>
@@ -34,8 +34,6 @@
 </template>
 
 <script lang="ts">
-import { getCurrentDatetimeString } from '@/common/classes/datetime';
-import { ESB_WEB } from '@/settings/consts/webs';
 import Web from '@/settings/interfaces/web';
 import UploadForm from '@/script/interfaces/UploadForm';
 import { Component, Vue } from 'vue-property-decorator';
@@ -43,14 +41,9 @@ import Bank from '@/settings/interfaces/bank';
 
 @Component({})
 export default class extends Vue {
-    private form: UploadForm = {
-        bank: undefined,
-        script: '',
-        notePrefix: ESB_WEB.name,
-        notePostfix: getCurrentDatetimeString(),
-        webs: [ESB_WEB],
-        isStable: true,
-    };
+    get uploadForm(): UploadForm {
+        return this.$store.state.scriptModule.uploadForm;
+    }
 
     get webs(): Web[] {
         return this.$store.state.settingsModule.webs;
@@ -62,20 +55,21 @@ export default class extends Vue {
 
     private handleScriptChange(event: InputEvent): void {
         const script: string = (event.target as HTMLTextAreaElement).value;
+        let uploadFormBank: undefined | Bank = undefined;
 
         for (const bank of this.banks) {
             if (script.indexOf(bank.name) !== -1) {
-                this.form.bank = bank;
-                return;
+                uploadFormBank = bank;
+                break;
             }
         }
 
-        this.form.bank = undefined;
+        this.$store.commit('scriptModule/setUploadFormBank', uploadFormBank);
     }
 
     private submit(): void {
         // TODO 實作送出功能
-        console.log('submit!', this.form);
+        console.log('submit!', this.uploadForm);
     }
 }
 </script>
--- src/script/store/mutations.ts
+++ src/script/store/mutations.ts
@@ -1,6 +1,11 @@
+import Bank from '@/settings/interfaces/bank';
 import { MutationTree } from 'vuex';
 import { ScriptState } from '@/script/store/state';
 
-const mutations: MutationTree<ScriptState> = {};
+const mutations: MutationTree<ScriptState> = {
+    setUploadFormBank(state: ScriptState, bank: Bank | undefined): void {
+        state.uploadForm.bank = bank;
+    },
+};
 
 export default mutations;
