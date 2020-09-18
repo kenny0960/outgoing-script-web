ad81d390bf42858eb77922612c0a29a260cd1301
Kenny <kenny_hung@mail.chungyo.net>
2020-09-18 14:07:35 +0800

    實作上傳腳本時鎖定表單的功能

--- src/upload/components/UploadForm.vue
+++ src/upload/components/UploadForm.vue
@@ -1,28 +1,33 @@
 <template>
     <a-form-model :model="uploadState" :label-col="{ span: 2 }" :wrapper-col="{ span: 20 }">
         <a-form-model-item label="網站">
-            <a-checkbox-group v-model="uploadState.webs">
+            <a-checkbox-group v-model="uploadState.webs" :disabled="isUploading">
                 <a-checkbox v-for="web in webs" :key="web.name" :value="web">
                     {{ web.name }}
                 </a-checkbox>
             </a-checkbox-group>
         </a-form-model-item>
         <a-form-model-item label="腳本">
-            <a-input v-model="uploadState.script" type="textarea" @change="handleScriptChange" />
+            <a-input
+                v-model="uploadState.script"
+                type="textarea"
+                :disabled="isUploading"
+                @change="handleScriptChange"
+            />
             <a-tag color="blue" v-if="uploadState.bank !== undefined">
                 {{ uploadState.bank.name }}
             </a-tag>
         </a-form-model-item>
         <a-form-model-item label="備註">
             <a-input-group compact>
-                <a-input v-model="uploadState.note" class="note" />
+                <a-input v-model="uploadState.note" class="note" :disabled="isUploading" />
             </a-input-group>
         </a-form-model-item>
         <a-form-model-item label="穩定版本">
-            <a-switch v-model="uploadState.isStable" />
+            <a-switch v-model="uploadState.isStable" :disabled="isUploading" />
         </a-form-model-item>
         <a-form-model-item :wrapper-col="{ offset: 2 }">
-            <a-button type="primary" @click="submit"> 上傳 </a-button>
+            <a-button type="primary" @click="submit" :loading="isUploading"> 上傳 </a-button>
             <a-button> 取消 </a-button>
         </a-form-model-item>
     </a-form-model>
@@ -37,6 +42,8 @@ import Bank from '@/settings/interfaces/bank';
 
 @Component({})
 export default class extends Vue {
+    private isUploading = false;
+
     get uploadState(): UploadState {
         return this.$store.state.uploadModule;
     }
@@ -64,6 +71,7 @@ export default class extends Vue {
     }
 
     private async submit(): Promise<void> {
+        this.isUploading = true;
         for (const web of this.uploadState.webs) {
             const script: Script = await this.$store.dispatch('uploadModule/createScript', web);
 
@@ -71,6 +79,7 @@ export default class extends Vue {
                 await this.$store.dispatch('uploadModule/setScriptStabled', { web, script });
             }
         }
+        this.isUploading = false;
     }
 }
 </script>
