aec78df2881b0f39d795dc37f6988b897bee9ce6
Kenny <kenny_hung@mail.chungyo.net>
2020-09-18 15:52:41 +0800

    實作驗證上傳腳本表單的功能

--- src/upload/components/UploadForm.vue
+++ src/upload/components/UploadForm.vue
@@ -1,13 +1,19 @@
 <template>
-    <a-form-model :model="uploadState" :label-col="{ span: 2 }" :wrapper-col="{ span: 20 }">
-        <a-form-model-item label="網站">
+    <a-form-model
+        ref="uploadForm"
+        :model="uploadState"
+        :rules="rules"
+        :label-col="{ span: 2 }"
+        :wrapper-col="{ span: 20 }"
+    >
+        <a-form-model-item label="網站" prop="webs">
             <a-checkbox-group v-model="uploadState.webs" :disabled="isUploading">
                 <a-checkbox v-for="web in webs" :key="web.name" :value="web">
                     {{ web.name }}
                 </a-checkbox>
             </a-checkbox-group>
         </a-form-model-item>
-        <a-form-model-item label="腳本">
+        <a-form-model-item label="腳本" prop="script">
             <a-input
                 v-model="uploadState.script"
                 type="textarea"
@@ -18,12 +24,12 @@
                 {{ uploadState.bank.name }}
             </a-tag>
         </a-form-model-item>
-        <a-form-model-item label="備註">
+        <a-form-model-item label="備註" prop="note">
             <a-input-group compact>
                 <a-input v-model="uploadState.note" class="note" :disabled="isUploading" />
             </a-input-group>
         </a-form-model-item>
-        <a-form-model-item label="穩定版本">
+        <a-form-model-item label="穩定版本" prop="isStable">
             <a-switch v-model="uploadState.isStable" :disabled="isUploading" />
         </a-form-model-item>
         <a-form-model-item :wrapper-col="{ offset: 2 }">
@@ -37,12 +43,25 @@
 import Script from '@/script/interfaces/Script';
 import Web from '@/settings/interfaces/web';
 import { UploadState } from '@/upload/store/state';
+import { FormModel } from 'ant-design-vue';
 import { Component, Vue } from 'vue-property-decorator';
 import Bank from '@/settings/interfaces/bank';
 
 @Component({})
 export default class extends Vue {
     private isUploading = false;
+    private rules = {
+        webs: [
+            {
+                type: 'array',
+                required: true,
+                message: '至少選擇一個網站',
+                trigger: 'blur',
+            },
+        ],
+        script: [{ required: true, message: '腳本不能為空', trigger: 'blur' }],
+        note: [{ required: true, message: '備註不能為空', trigger: 'blur' }],
+    };
 
     get uploadState(): UploadState {
         return this.$store.state.uploadModule;
@@ -71,15 +90,21 @@ export default class extends Vue {
     }
 
     private async submit(): Promise<void> {
-        this.isUploading = true;
-        for (const web of this.uploadState.webs) {
-            const script: Script = await this.$store.dispatch('uploadModule/createScript', web);
+        (this.$refs.uploadForm as FormModel).validate(
+            async (isValid: boolean): Promise<void> => {
+                if (isValid) {
+                    this.isUploading = true;
+                    for (const web of this.uploadState.webs) {
+                        const script: Script = await this.$store.dispatch('uploadModule/createScript', web);
 
-            if (this.uploadState.isStable) {
-                await this.$store.dispatch('uploadModule/setScriptStabled', { web, script });
+                        if (this.uploadState.isStable) {
+                            await this.$store.dispatch('uploadModule/setScriptStabled', { web, script });
+                        }
+                    }
+                    this.isUploading = false;
+                }
             }
-        }
-        this.isUploading = false;
+        );
     }
 }
 </script>
