4c35474e7a37f8fcaa201a0543886ed198e55608
Kenny <kenny_hung@mail.chungyo.net>
2020-09-22 18:37:13 +0800

    新增 vue-fragment 套件

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import 'ant-design-vue/dist/antd.css';
 import App from './app/components/App.vue';
 import router from './app/router';
 import store from './app/store';
+import { Plugin } from 'vue-fragment';
 
 Vue.config.productionTip = false;
 
 Vue.use(Antd);
+Vue.use(Plugin);
 
 new Vue({
     router,
