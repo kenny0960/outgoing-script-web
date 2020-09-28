f4cba9c3c9e547e61be39b8924173708624c5288
Kenny <kenny_hung@mail.chungyo.net>
2020-09-28 16:00:40 +0800

    新增透過銀行代碼取得銀行的函式

--- src/settings/store/getters.ts
+++ src/settings/store/getters.ts
@@ -1,7 +1,18 @@
+import { DEFAULT_BANK } from '@/settings/consts/banks';
+import Bank from '@/settings/interfaces/bank';
 import { GetterTree } from 'vuex';
 import RootState from '@/app/store/state';
 import { SettingsState } from '@/settings/store/state';
 
-const getters: GetterTree<SettingsState, RootState> = {};
+const getters: GetterTree<SettingsState, RootState> = {
+    bankBySwiftBankCode: ({ banks }: SettingsState) => (swiftBankCode: string): Bank => {
+        for (const bank of banks) {
+            if (bank.swiftBankCode === swiftBankCode) {
+                return bank;
+            }
+        }
+        return DEFAULT_BANK;
+    },
+};
 
 export default getters;
