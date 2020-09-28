6087231840b5bce03b41f7d6a233c6335e96b6fa
Kenny <kenny_hung@mail.chungyo.net>
2020-09-28 17:20:36 +0800

    新增剪貼簿套件

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import App from './app/components/App.vue';
 import router from './app/router';
 import store from './app/store';
 import { Plugin } from 'vue-fragment';
+import VueClipboard from 'vue-clipboard2';
 
 Vue.config.productionTip = false;
 
 Vue.use(Antd);
 Vue.use(Plugin);
+Vue.use(VueClipboard);
 
 new Vue({
     router,
