c7049bd76de40263b460ae535bdefacef4360c69
Kenny <kenny_hung@mail.chungyo.net>
2020-09-28 15:59:13 +0800

    簡化腳本列表主頁面取得列表的邏輯

--- src/script/components/Main.vue
+++ src/script/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-    <a-skeleton v-if="isScriptsLoading" active :paragraph="{ rows: 5 }" />
+    <a-skeleton v-if="isLoading" active :paragraph="{ rows: 5 }" />
 </template>
 
 <script lang="ts">
@@ -8,15 +8,11 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({})
 export default class extends Vue {
     public mounted(): void {
-        this.fetchScripts();
-    }
-
-    get isScriptsLoading(): boolean {
-        return this.$store.getters['scriptModule/isLoading'];
-    }
-
-    public fetchScripts(): void {
         this.$store.dispatch('scriptModule/fetchScripts');
+    }
+
+    get isLoading(): boolean {
+        return this.$store.getters['scriptModule/isLoading'];
     }
 }
 </script>
