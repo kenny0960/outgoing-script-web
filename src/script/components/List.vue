2d3ce6cfb0bf414d58ea1465727b7d36ce22bfe7
Kenny <kenny_hung@mail.chungyo.net>
2020-10-07 16:46:01 +0800

    美化穩定版本腳本的樣式

--- src/script/components/List.vue
+++ src/script/components/List.vue
@@ -1,7 +1,7 @@
 <template>
     <a-list :grid="{ gutter: 16, column: 4 }" :data-source="scripts">
         <a-list-item slot="renderItem" slot-scope="script">
-            <a-card>
+            <a-card :class="script.isStabled ? 'stabled-script' : ''">
                 <slot slot="title">
                     <a-tooltip v-if="verified(script)">
                         <template slot="title">已驗證</template>
@@ -38,7 +38,7 @@
                 <a-descriptions :column="1" class="script-detail">
                     <a-descriptions-item label="檔案大小"> {{ size(script) }} KB </a-descriptions-item>
                     <a-descriptions-item v-if="script.note">
-                        <a-tag color="blue">{{ script.note }}</a-tag>
+                        <a-tag>{{ script.note }}</a-tag>
                     </a-descriptions-item>
                 </a-descriptions>
             </a-card>
@@ -90,6 +90,24 @@ export default class extends Vue {
 </style>
 
 <style>
+.stabled-script {
+    border-color: #91d5ff;
+    font-weight: bold;
+}
+.stabled-script .ant-card-head {
+    border-color: #91d5ff;
+}
+.stabled-script .ant-card-actions {
+    border-color: #91d5ff;
+}
+.stabled-script .ant-tag {
+    color: #1890ff;
+    background: #e6f7ff;
+    border-color: #91d5ff;
+}
+.stabled-script .ant-card-actions .anticon {
+    color: #1890ff;
+}
 .script-detail .ant-descriptions-row:last-child {
     text-align: right;
 }
