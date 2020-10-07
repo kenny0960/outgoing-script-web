dad2b8f22bcb3b18e36a3df05d46e92943cd9b75
Kenny <kenny_hung@mail.chungyo.net>
2020-10-07 14:56:05 +0800

    實作點擊分頁的功能

--- src/script/components/Pagination.vue
+++ src/script/components/Pagination.vue
@@ -4,16 +4,27 @@
         :pageSize="pagination.pageSize"
         :current="pagination.current"
         :total="pagination.total"
+        @change="handleCurrentChange"
     />
 </template>
 
 <script lang="ts">
+import Pagination from '@/script/interfaces/Pagination';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({})
 export default class extends Vue {
-    get pagination(): boolean {
+    get pagination(): Pagination {
         return this.$store.state.scriptModule.pagination;
+    }
+
+    private handleCurrentChange(current: number) {
+        this.$store.commit('scriptModule/setPagination', {
+            ...this.pagination,
+            current: current,
+        });
+        this.$store.commit('scriptModule/setScripts', undefined);
+        this.$store.dispatch('scriptModule/fetchScripts');
     }
 }
 </script>
--- src/script/store/actions.ts
+++ src/script/store/actions.ts
@@ -1,4 +1,5 @@
 import RootState from '@/app/store/state';
+import Pagination from '@/script/interfaces/Pagination';
 import Script from '@/script/interfaces/Script';
 import { message } from 'ant-design-vue';
 import axios, { AxiosResponse } from 'axios';
@@ -6,11 +7,13 @@ import { ActionTree } from 'vuex';
 import { ScriptState } from '@/script/store/state';
 
 const actions: ActionTree<ScriptState, RootState> = {
-    async fetchScripts({ rootState, commit }): Promise<void> {
+    async fetchScripts({ state, rootState, commit }): Promise<void> {
+        const pagination: Pagination = state.pagination;
+
         try {
             const response: AxiosResponse = await axios({
                 method: 'GET',
-                url: `${rootState.tabModule.selectedWeb.uri}/api/v1/banks/${rootState.tabModule.selectedBank.swiftBankCode}/scripts`,
+                url: `${rootState.tabModule.selectedWeb.uri}/api/v1/banks/${rootState.tabModule.selectedBank.swiftBankCode}/scripts?page=${pagination.current}&per_page=${pagination.pageSize}`,
                 headers: {
                     Authorization: rootState.tabModule.selectedWeb.token,
                 },
@@ -34,6 +37,10 @@ const actions: ActionTree<ScriptState, RootState> = {
             }
 
             commit('setScripts', scripts);
+            commit('setPagination', {
+                ...pagination,
+                total: parseInt(response.headers['x-total']),
+            });
         } catch (error) {
             message.error(
                 `取得「${rootState.tabModule.selectedWeb.name} - ${rootState.tabModule.selectedBank.name}」腳本失敗：${error.message}`
