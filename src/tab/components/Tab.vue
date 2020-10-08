054b700001e26bbd249772d7bfdb42a5985583ea
Kenny <kenny_hung@mail.chungyo.net>
2020-10-08 16:06:40 +0800

    顯示穩定版本腳本的資訊至銀行標籤頁上

--- src/tab/components/Tab.vue
+++ src/tab/components/Tab.vue
@@ -1,21 +1,32 @@
 <template>
-    <a-tabs :active-key="selectedBank.swiftBankCode" @tabClick="handleBankClick">
+    <a-tabs class="tab" :active-key="selectedBank.swiftBankCode" @tabClick="handleBankClick">
         <slot slot="tabBarExtraContent">
             <a-radio-group button-style="solid" :defaultValue="selectedWeb" @change="handleWebClick">
                 <a-radio-button v-for="web in webs" :value="web" :key="web.name">{{ web.name }}</a-radio-button>
             </a-radio-group>
         </slot>
-        <a-tab-pane v-for="bank in banks" :key="bank.swiftBankCode" :tab="bank.swiftBankCode" />
+        <a-tab-pane v-for="(bank, index) in banks" :key="bank.swiftBankCode">
+            <a-statistic
+                slot="tab"
+                :value="`${bank.swiftBankCode}(${stableScripts[index].revision})`"
+                :title="stableScripts[index].note"
+            />
+        </a-tab-pane>
     </a-tabs>
 </template>
 
 <script lang="ts">
+import Script from '@/script/interfaces/Script';
 import Bank from '@/settings/interfaces/bank';
 import Web from '@/settings/interfaces/web';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({})
 export default class extends Vue {
+    get stableScripts(): Script[] {
+        return this.$store.state.stableScriptModule.stableScripts;
+    }
+
     get webs(): Web[] {
         return this.$store.state.settingsModule.webs;
     }
@@ -54,3 +65,12 @@ export default class extends Vue {
     }
 }
 </script>
+
+<style>
+.tab .ant-statistic-content {
+    font-size: 16px;
+}
+.tab .ant-statistic-title {
+    font-size: 12px;
+}
+</style>
