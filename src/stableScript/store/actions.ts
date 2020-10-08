57ec2402d4e7449e479e45a8bc27e33491ff0432
Kenny <kenny_hung@mail.chungyo.net>
2020-10-08 15:52:43 +0800

    實作取得穩定版本腳本的 ajax

--- src/stableScript/store/actions.ts
+++ src/stableScript/store/actions.ts
@@ -1,7 +1,32 @@
 import RootState from '@/app/store/state';
+import Script from '@/script/interfaces/Script';
+import Bank from '@/settings/interfaces/bank';
+import { message } from 'ant-design-vue';
+import axios, { AxiosResponse } from 'axios';
 import { ActionTree } from 'vuex';
 import { StableScriptState } from '@/stableScript/store/state';
 
-const actions: ActionTree<StableScriptState, RootState> = {};
+const actions: ActionTree<StableScriptState, RootState> = {
+    async fetchStableScript({ rootState, commit }, bank: Bank): Promise<void> {
+        try {
+            const response: AxiosResponse = await axios({
+                method: 'GET',
+                url: `${rootState.tabModule.selectedWeb.uri}/api/v1/banks/${bank.swiftBankCode}/scripts/stable`,
+                headers: {
+                    Authorization: rootState.tabModule.selectedWeb.token,
+                },
+            });
+
+            if (response.status === 200) {
+                const stableScript: Script = response.data;
+                commit('setStableScript', stableScript);
+            }
+        } catch (error) {
+            message.error(
+                `取得「${rootState.tabModule.selectedWeb.name} - ${bank.name}」穩定版本腳本失敗：${error.message}`
+            );
+        }
+    },
+};
 
 export default actions;
--- src/stableScript/store/mutations.ts
+++ src/stableScript/store/mutations.ts
@@ -1,6 +1,11 @@
 import { MutationTree } from 'vuex';
 import { StableScriptState } from '@/stableScript/store/state';
+import Script from '@/script/interfaces/Script';
 
-const mutations: MutationTree<StableScriptState> = {};
+const mutations: MutationTree<StableScriptState> = {
+    setStableScript(state: StableScriptState, stableScript: Script): void {
+        state.stableScripts.push(stableScript);
+    },
+};
 
 export default mutations;
